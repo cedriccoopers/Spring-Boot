4ae80fb66ffddaff6c4cb1742c4998576c2af3c7
Cedric <cedriccoopers@gmail.com>
2024-08-13 20:31:34 +0200

    Feat: adding skeleton spring boot app

---
+++ src/main/java/com/example/cedric_learn_springboot/CedricLearnSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.cedric_learn_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CedricLearnSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CedricLearnSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cedric_learn_springboot/CedricLearnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cedric_learn_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CedricLearnSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
